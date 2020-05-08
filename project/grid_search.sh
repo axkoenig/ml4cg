2a49eaf18f79da9e04ac41c8b136e6ab997aa3c0
ga96qov <li.nguyen@tum.de>
2020-05-08 22:33:52 +0200

    Fix typo in grid search

--- project/grid_search.sh
+++ project/grid_search.sh
@@ -10,7 +10,7 @@ for batch_size in 16 32 64
 do
     # for gamma in 0.01 0.4 1 1.5 2
     for lambda_g in 0.01 0.4 1 1.5 2
-        for lambda_g in 0.01 0.4 1 1.5 2
+        for lambda_f in 0.01 0.4 1 1.5 2
         do
             ( echo "Training G2G with --batch_size=$batch_size --gamma=$gamma --lambda_g=$lambda_g --lambda_d=$lambda_d" && \
             CUDA_VISIBLE_DEVICES=0,1 python train.py --gpus=$gpus --batch_size=$batch_size --gamma=$gamma --lambda_g=$lambda_g --lambda_d=$lambda_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_gamma${gamma}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
