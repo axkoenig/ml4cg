ed27e2fbebda2634085edac488bac973f8a3f63f
ga96qov <li.nguyen@tum.de>
2020-05-08 23:02:12 +0200

    Adjust grid search (will otherwise take too long to train)

--- project/grid_search.sh
+++ project/grid_search.sh
@@ -5,16 +5,13 @@ conda activate team6
 
 max_epochs=20
 gpus=2
+batch_size=32
 
-for batch_size in 16 32 64
+for lambda_g in 0.01 0.4 1 1.5 10
 do
-    # for gamma in 0.01 0.4 1 1.5 2
-    for lambda_g in 0.01 0.4 1 1.5 2
+    for lambda_d in 0.01 0.4 1 1.5 10
     do
-        for lambda_d in 0.01 0.4 1 1.5 2
-        do
-            ( echo "Training G2G with --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d" && \
-            CUDA_VISIBLE_DEVICES=0,1 python train.py --gpus=$gpus --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
-        done
+        ( echo "Training G2G with --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d" && \
+        CUDA_VISIBLE_DEVICES=0,1 python train.py --gpus=$gpus --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
     done
 done
