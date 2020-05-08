b60c975702dc878519e50e4652b2af006c39853b
ga96qov <li.nguyen@tum.de>
2020-05-08 22:40:02 +0200

    Adjust grid search

--- project/grid_search.sh
+++ project/grid_search.sh
@@ -13,8 +13,8 @@ do
     do
         for lambda_d in 0.01 0.4 1 1.5 2
         do
-            ( echo "Training G2G with --batch_size=$batch_size --gamma=$gamma --lambda_g=$lambda_g --lambda_d=$lambda_d" && \
-            CUDA_VISIBLE_DEVICES=0,1 python train.py --gpus=$gpus --batch_size=$batch_size --gamma=$gamma --lambda_g=$lambda_g --lambda_d=$lambda_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_gamma${gamma}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
+            ( echo "Training G2G with --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d" && \
+            CUDA_VISIBLE_DEVICES=0,1 python train.py --gpus=$gpus --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
         done
     done
 done
