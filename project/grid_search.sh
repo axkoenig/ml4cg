0ffb91ee0ad3dbddab84df272d343303486f334f
ga96qov <li.nguyen@tum.de>
2020-05-08 23:28:00 +0200

    Changing cuda devices (others are full)

--- project/grid_search.sh
+++ project/grid_search.sh
@@ -7,11 +7,11 @@ max_epochs=20
 gpus=2
 batch_size=32
 
-for lambda_g in 0.01 0.4 1 1.5 10
+for lambda_g in 0.01 1 10
 do
-    for lambda_d in 0.01 0.4 1 1.5 10
+    for lambda_d in 0.01 1 10
     do
         ( echo "Training G2G with --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d" && \
-        CUDA_VISIBLE_DEVICES=0,1 python train.py --gpus=$gpus --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
+        CUDA_VISIBLE_DEVICES=5,6 python train.py --gpus=$gpus --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
     done
 done
