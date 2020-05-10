28f5ead71e17c2c2eec1a1e942c61a757d89f4aa
ga96qov <li.nguyen@tum.de>
2020-05-10 21:27:58 +0200

    training on GPU 2 and 3

--- project/grid_search.sh
+++ project/grid_search.sh
@@ -12,4 +12,4 @@ lambda_g=0.5
 lambda_d=0.005
 
 ( echo "Training G2G with --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d and --lr_d=$lr_d" && \
-CUDA_VISIBLE_DEVICES=3,4 python train.py --gpus=$gpus --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d --lr_g=$lr_g --lr_d=$lr_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
+CUDA_VISIBLE_DEVICES=2,3 python train.py --gpus=$gpus --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d --lr_g=$lr_g --lr_d=$lr_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
