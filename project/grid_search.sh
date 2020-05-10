0b22a9d2f6881948cd0b7c31636ff99077a47fd6
ga96qov <li.nguyen@tum.de>
2020-05-10 21:05:09 +0200

    Run for visual results for presentation on Monday

--- project/grid_search.sh
+++ project/grid_search.sh
@@ -6,13 +6,10 @@ conda activate team6
 max_epochs=20
 gpus=2
 batch_size=16
-lr_d=0.0001
+lr_d=0.00005
+lt_g=0.0002
+lambda_g=0.5
+lambda_d=0.005
 
-for lambda_g in 0.01 1 10
-do
-    for lambda_d in 0.01 1 10
-    do
-        ( echo "Training G2G with --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d" && \
-        CUDA_VISIBLE_DEVICES=0,1 python train.py --gpus=$gpus --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d --lr_g=$lr_g --lr_d=$lr_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
-    done
-done
+( echo "Training G2G with --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d and --lr_d=$lr_d" && \
+CUDA_VISIBLE_DEVICES=3,4 python train.py --gpus=$gpus --batch_size=$batch_size --lambda_g=$lambda_g --lambda_d=$lambda_d --lr_g=$lr_g --lr_d=$lr_d --max_epochs=$max_epochs 2>&1 ) | tee grid_logs/bs${batch_size}_lambda_g${lambda_g}_lambda_d${lambda_d}.log
