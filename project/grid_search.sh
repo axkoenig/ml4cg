eb9c3f595b4f7c915f2c77aace15072af9c415a6
Alexander Koenig <koenig@localhost.localdomain>
2020-06-24 18:00:28 +0300

    New grid search

--- project/grid_search.sh
+++ project/grid_search.sh
@@ -3,13 +3,14 @@
 eval "$(conda shell.bash hook)"
 
 max_epochs=10
-gpus=4
-batch_size=8
+gpus=5
+batch_size=2
 lr_gen=0.0001
 lr_dis=0.0001
+num_workers=1
 
-for delta in 5.0 20.0
+for delta in 1.0
 do
     ( echo "Training G2G with --delta=$delta" && \
-    CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py --lr_gen=$lr_gen --lr_dis=$lr_dis --log_name=grid_delta_$delta --delta=$delta --gpus=$gpus --max_epochs=$max_epochs --batch_size=$batch_size 2>&1 ) | tee logs/grid_logs/delta_${delta}.log 
+    CUDA_VISIBLE_DEVICES=0,2,4,5,7 python train.py --num_workers=$num_workers --lr_gen=$lr_gen --lr_dis=$lr_dis --log_name=mser_resnorm_delta_$delta --delta=$delta --gpus=$gpus --max_epochs=$max_epochs --batch_size=$batch_size 2>&1 ) | tee logs/grid_logs/delta_${delta}_resnorm.log 
 done
