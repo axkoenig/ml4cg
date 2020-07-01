4d48e76cc7f6c740d3af6075850baa3e7859f19d
Alexander Koenig <koenig@localhost.localdomain>
2020-07-01 19:47:39 +0300

    Update grid search

--- project/grid_search.sh
+++ project/grid_search.sh
@@ -2,15 +2,18 @@
 
 eval "$(conda shell.bash hook)"
 
-max_epochs=10
-gpus=5
-batch_size=2
+max_epochs=6
+gpus=4
+batch_size=4
 lr_gen=0.0001
 lr_dis=0.0001
-num_workers=1
+num_plot_triplets=2
 
-for delta in 1.0
+for gamma in 1000 100
 do
-    ( echo "Training G2G with --delta=$delta" && \
-    CUDA_VISIBLE_DEVICES=0,2,4,5,7 python train.py --num_workers=$num_workers --lr_gen=$lr_gen --lr_dis=$lr_dis --log_name=mser_resnorm_delta_$delta --delta=$delta --gpus=$gpus --max_epochs=$max_epochs --batch_size=$batch_size 2>&1 ) | tee logs/grid_logs/delta_${delta}_resnorm.log 
+    for delta_max in 1.0 5.0
+    do
+        ( echo "Training G2G with --delta_max=$delta_max --gamma=$gamma" && \
+        CUDA_VISIBLE_DEVICES=0,1,3,4 python train.py --num_plot_triplets=$num_plot_triplets --lr_gen=$lr_gen --lr_dis=$lr_dis --log_name=final_maxdelta${delta_max}_gamma${gamma} --gamma=$gamma --delta_max=$delta_max --gpus=$gpus --max_epochs=$max_epochs --batch_size=$batch_size 2>&1 ) | tee logs/grid_logs/final_maxdelta${delta_max}_gamma${gamma}.log 
+    done
 done
